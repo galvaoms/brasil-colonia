cc741c0b08f03a744c41019cfe890666638ae781
galvaoms <141883777+galvaoms@users.noreply.github.com>
2023-08-09 22:30:37 -0300

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    question: "O que é degredo?",
+    answers: [
+      { option: "Uma forma de comunicação por meio de gestos", correct: false },
+      { option: "Um tipo de construção arquitetônica antiga", correct: false },
+      { option: "Um castigo que envolvia o exílio forçado de criminosos para colônias distantes", correct: true },
+      { option: "Uma técnica de agricultura sustentável", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a diferença entre inconfidência e conjuração?",
+    answers: [
+      { option: "A inconfidência era um movimento de revolta no Brasil colonial, enquanto a conjuração era um sistema de governo", correct: false },
+      { option: "A inconfidência era uma aliança militar, enquanto a conjuração era um levante político", correct: false },
+      { option: "A inconfidência era uma conspiração contra a coroa portuguesa, enquanto a conjuração era uma festa popular", correct: true },
+      { option: "A inconfidência era uma reforma religiosa, enquanto a conjuração era um movimento artístico", correct: false },
+    ],
+  },
+  {
+    question: "Em que período aconteceu a Inconfidência Mineira?",
+    answers: [
+      { option: "Século XVII", correct: false },
+      { option: "Século XVIII", correct: true },
+      { option: "Século XIX", correct: false },
+      { option: "Século XX", correct: false },
+    ],
+  },
+  {
+    question: "Quem foi o denunciante da Inconfidência Mineira?",
+    answers: [
+      { option: "Joaquim José da Silva Xavier", correct: false },
+      { option: "Álvaro  de Araújo Antunes", correct: false },
+      { option: "Cláudio Manuel da Costa", correct: false },
+      { option: "Joaquim Silvério dos Reis", correct: true },
+    ],
+  },
+  {
+    question: "Quem compunha o grupo dos inconfidentes?",
+    answers: [
+      { option: "Escravizados, pessoas de baixa renda, pequenos comerciantes", correct: false },
+      { option: "Elite socioeconômica, comerciantes, fazendeiros, médicos, militares", correct: true },
+      { option: "Membros da igreja, padres, fiéis", correct: false },
+      { option: "Somente militares", correct: false },
+    ],
+  },
+];
